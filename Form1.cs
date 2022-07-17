0373e75c6a54b5176f2b131361682d87add1a159
Terry Keeton <terrykeeton97@outlook.com>
2022-07-17 08:32:49 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Hotkeys_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Hotkeys.Init(comboBox1);
+        }
+
+        protected override void WndProc(ref Message hotkey)
+        {
+            bool Visible = this.Visible;
+            base.WndProc(ref hotkey);
+
+            if (Visible && hotkey.Msg == 0x0312)
+            {
+                //your code here
+                MessageBox.Show("Global hotkey pressed");
+            }
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Hotkeys.SetHotKey(comboBox1, Handle);
+        }
+    }
+}
---
+++ Hotkeys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Hotkeys_Example
+{
+    internal class Hotkeys
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk); //handle, Id of hotkey, modifier (e.g ALT + DEL), hotkey key
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public static void Init(ComboBox comboBox)
+        {
+            var hotkeys = new List<string>() { "DEL", "INSERT", "HOME" };
+            comboBox.DataSource = hotkeys;
+        }
+
+        public static void SetHotKey(ComboBox comboBox, IntPtr hWnd)
+        {
+            UnregisterHotKey(hWnd, 0);
+
+            if (comboBox.Text == "DEL")
+                RegisterHotKey(hWnd, 0, 0, Keys.Delete.GetHashCode());
+
+            if (comboBox.Text == "INSERT")
+                RegisterHotKey(hWnd, 0, 0, Keys.Insert.GetHashCode());
+
+            if (comboBox.Text == "HOME")
+                RegisterHotKey(hWnd, 0, 0, Keys.Home.GetHashCode());
+        }
+    }
+}
